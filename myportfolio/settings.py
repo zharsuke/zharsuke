5efe3b2cf745a5619e402404effc5df954daa912
Al Azhar <alazharisqi@gmail.com>
2021-06-11 17:29:33 +0700

    edit

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -134,7 +134,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
